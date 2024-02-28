5556d4c0fa46a579a6fd5331f653bf8849cba870
Damien Sedgwick <damienksedgwick@gmail.com>
2024-02-28 21:42:17 +0000

    started fleshing out cli progam

--- napp.go
+++ napp.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+type name struct {
+	NotEnoughArguments string
+}
 
 func main() {
-	fmt.Println("Hello from napp!")
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Println("No args provided!")
+		os.Exit(1)
+	}
+
+	if len(args) > 1 {
+		fmt.Println("Too many args provided!")
+		os.Exit(1)
+	}
+
+	name := args[0]
+	// TODO: check for illegal characters
+
+	fmt.Println("Project Name: ", name)
 }
