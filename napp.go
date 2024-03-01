29a592684bda82d2f8d4b982e4efbd449c992a3c
Damien Sedgwick <damienksedgwick@gmail.com>
2024-03-01 13:03:34 +0000

    refactored, added helper functions and error messages

--- napp.go
+++ napp.go
@@ -1,24 +1,61 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"regexp"
+)
+
+// Define custom errors
+var (
+	ErrNoArguments      = errors.New("no arguments provided")
+	ErrTooManyArguments = errors.New("too many arguments provided")
+	ErrInvalidName      = errors.New("invalid project name")
 )
 
 func main() {
 	args := os.Args[1:]
-	if len(args) == 0 {
-		fmt.Println("No args provided!")
-		os.Exit(1)
-	}
 
-	if len(args) > 1 {
-		fmt.Println("Too many args provided!")
+	if err := validateArgs(args); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
 	name := args[0]
-	// TODO: check for illegal characters
-
 	fmt.Println("Project Name: ", name)
 }
+
+func validateArgs(args []string) error {
+	if len(args) == 0 {
+		return ErrNoArguments
+	}
+
+	if len(args) > 1 {
+		return ErrTooManyArguments
+	}
+
+	name := args[0]
+	if name == "--help" {
+		fmt.Println("You can create a new Nano App with the command 'napp <project-name>'")
+		os.Exit(0)
+	}
+
+	if isInvalidProjectName(name) {
+		return ErrInvalidName
+	}
+
+	return nil
+}
+
+func isInvalidProjectName(name string) bool {
+	pattern := "^[a-z0-9-]+$"
+
+	matched, err := regexp.MatchString(pattern, name)
+	if err != nil {
+		return true
+	}
+
+	return !matched
+}
+
