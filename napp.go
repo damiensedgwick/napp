5b1277a91e6ea8228e86ace2524baec59b5b81cf
Damien Sedgwick <damienksedgwick@gmail.com>
2024-03-26 21:58:52 +0000

    replaced main.go template with net/http, replacing echo

--- napp.go
+++ napp.go
@@ -102,32 +102,17 @@ import (
 	"fmt"
 	"html/template"
 	"io"
+	"log"
+	"net/http"
 	"os"
 	"time"
 
 	"github.com/gorilla/sessions"
 	"github.com/joho/godotenv"
-	"github.com/labstack/echo-contrib/session"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
-
-type Template struct {
-	tmpl *template.Template
-}
-
-func newTemplate() *Template {
-	return &Template{
-		tmpl: template.Must(template.ParseGlob("template/*.html")),
-	}
-}
-
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.tmpl.ExecuteTemplate(w, name, data)
-}
 
 func main() {
 	err := godotenv.Load(".env")
@@ -135,76 +120,76 @@ func main() {
 		fmt.Println("error loading godotenv")
 	}
 
-	e := echo.New()
-
-	e.Static("/static", "static")
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	e.Use(middleware.Secure())
-
-	store := sessions.NewCookieStore([]byte(os.Getenv("%s")))
-	e.Use(session.Middleware(store))
-
-	e.Renderer = newTemplate()
+	r := http.NewServeMux()
+	fs := http.FileServer(http.Dir("./static"))
+	r.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	db, err := gorm.Open(sqlite.Open(os.Getenv("%s")), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 
-	// Migrate the schema
 	db.AutoMigrate(&User{})
+	createInitialUser(db)
 
-	var user User
-	notFoundErr := db.First(&user, "email = ?", "johnsnow@winterfell.com").Error
-	if notFoundErr == gorm.ErrRecordNotFound {
-		// User not found; proceed with creation
-		db.Create(&User{
-			Model:     gorm.Model{},
-			Name:      "John Snow",
-			Email:     "johnsnow@winterfell.com",
-			Password:  "$2a$10$1oPDSctekA8P2IHDHoKNb.JjWJ4XFwzZAvYSHp0s4byhFeMp9.da.",
-			CreatedAt: time.Time{},
-			UpdatedAt: &time.Time{},
-		})
-	}
+	store := sessions.NewCookieStore([]byte(os.Getenv("%s")))
 
-	e.GET("/", func(c echo.Context) error {
-		sess, _ := session.Get("session", c)
+	stack := CreateMiddlewareStack(
+		Logging,
+	)
 
-		if sess.Values["user"] != nil {
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		t := newTemplate()
+
+    session, _ := store.Get(r, "session")
+		if session.Values["user"] != nil {
 			var user User
 
-			err := json.Unmarshal(sess.Values["user"].([]byte), &user)
+			err := json.Unmarshal(session.Values["user"].([]byte), &user)
 			if err != nil {
 				fmt.Println("error unmarshalling user value")
-				return err
 			}
 
-			return c.Render(200, "index", newPageData(user))
+			t.Render(w, "index", newPageData(user))
+			return
 		}
 
-		return c.Render(200, "index", nil)
+		var data interface{}
+
+		t.Render(w, "index", data)
+		return
 	})
 
-	e.GET("/auth/sign-in", func(c echo.Context) error {
-		return c.Render(200, "auth-form", nil)
+	r.HandleFunc("GET /auth/sign-in", func(w http.ResponseWriter, r *http.Request) {
+		t := newTemplate()
+
+		var data interface{}
+
+		t.Render(w, "auth-form", data)
+		return
 	})
 
-	e.POST("/auth/sign-in", func(c echo.Context) error {
-		email := c.FormValue("email")
-		password := c.FormValue("password")
+	r.HandleFunc("POST /auth/sign-in", func(w http.ResponseWriter, r *http.Request) {
+		t := newTemplate()
 
-		// Read
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Error parsing form data", http.StatusBadRequest)
+		}
+
+		email := r.Form.Get("email")
+		password := r.Form.Get("password")
+
 		var user User
 		db.First(&user, "email = ?", email)
 
 		if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
-			return echo.ErrUnauthorized
+			http.Error(w, "Unauthorised", http.StatusUnauthorized)
+			return
 		}
 
-		sess, _ := session.Get("session", c)
-		sess.Options = &sessions.Options{
+		session, _ := store.Get(r, "session")
+		session.Options = &sessions.Options{
 			Path:     "/",
 			MaxAge:   86400 * 7,
 			HttpOnly: true,
@@ -213,33 +198,96 @@ func main() {
 		userBytes, err := json.Marshal(user)
 		if err != nil {
 			fmt.Println("error marshalling user value")
-			return err
 		}
 
-		sess.Values["user"] = userBytes
+		session.Values["user"] = userBytes
 
-		err = sess.Save(c.Request(), c.Response())
+		err = session.Save(r, w)
 		if err != nil {
 			fmt.Println("error saving session: ", err)
-			return err
 		}
 
-		return c.Render(200, "index", newPageData(user))
+		t.Render(w, "index", newPageData(user))
+		return
 	})
 
-	e.POST("/auth/sign-out", func(c echo.Context) error {
-		sess, _ := session.Get("session", c)
+	r.HandleFunc("POST /auth/sign-out", func(w http.ResponseWriter, r *http.Request) {
+		t := newTemplate()
+
+		sess, _ := store.Get(r, "session")
 		sess.Options.MaxAge = -1
-		err := sess.Save(c.Request(), c.Response())
+		err := sess.Save(r, w)
 		if err != nil {
 			fmt.Println("error saving session")
-			return err
 		}
 
-		return c.Render(200, "index", nil)
+		var data interface{}
+
+		t.Render(w, "index", data)
+		return
 	})
 
-	e.Logger.Fatal(e.Start(":8080"))
+	s := http.Server{
+		Addr:    ":8080",
+		Handler: stack(r),
+	}
+
+	fmt.Println("Running server on localhost:8080")
+	if err := s.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type Template struct {
+	tmpl *template.Template
+}
+
+func newTemplate() *Template {
+	return &Template{
+		tmpl: template.Must(
+			template.ParseGlob("template/*.html")),
+	}
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}) error {
+	return t.tmpl.ExecuteTemplate(w, name, data)
+}
+
+type Middleware func(http.Handler) http.Handler
+
+func CreateMiddlewareStack(xs ...Middleware) Middleware {
+	return func(next http.Handler) http.Handler {
+		for i := len(xs) - 1; i >= 0; i-- {
+			x := xs[i]
+			next = x(next)
+		}
+		return next
+	}
+}
+
+type wrappedWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (w *wrappedWriter) WriteHeader(statusCode int) {
+	w.ResponseWriter.WriteHeader(statusCode)
+	w.statusCode = statusCode
+}
+
+func Logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		wrapped := &wrappedWriter{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+
+		next.ServeHTTP(wrapped, r)
+
+		log.Println(r.Method, r.URL.Path, wrapped.statusCode, time.Since(start))
+	})
 }
 
 type PageData struct {
@@ -260,6 +308,22 @@ type User struct {
 	CreatedAt time.Time
 	UpdatedAt *time.Time
 }
+
+func createInitialUser(db *gorm.DB) {
+	var user User
+	err := db.First(&user, "email = ?", "johnsnow@winterfell.com").Error
+	if err == gorm.ErrRecordNotFound {
+		db.Create(&User{
+			Model:     gorm.Model{},
+			Name:      "John Snow",
+			Email:     "johnsnow@winterfell.com",
+			Password:  "$2a$10$1oPDSctekA8P2IHDHoKNb.JjWJ4XFwzZAvYSHp0s4byhFeMp9.da.",
+			CreatedAt: time.Time{},
+			UpdatedAt: &time.Time{},
+		})
+	}
+}
+
 `, sessEnv, dbEnv)
 
 	filePath := filepath.Join(projectName, "cmd", "main.go")
