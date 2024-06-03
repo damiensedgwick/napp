3cb4a4f494eb277e682a0a1b3508b63bd8bc039c
Damien Sedgwick <damien@elsewhen.com>
2024-06-03 20:28:08 +0100

    fixed wording

--- napp.go
+++ napp.go
@@ -17,7 +17,7 @@ func main() {
 	app := &cli.App{
 		Name:      "napp",
 		UsageText: "[command] [command options]",
-		Version:   "v1.0.1",
+		Version:   "v1.0.2",
 		Description: `A command line tool that bootstraps Go, HTMX and SQLite web
 	 applications and Dockerises them for ease of deployment`,
 		Commands: []cli.Command{
@@ -640,7 +640,7 @@ func createHtmlFile(projectName string) {
       <input id="password" class="auth-form__input" type="password" name="password" value="" required>
     </div>
 
-    <button class="btn auth-form__btn" type="submit">Sign In</button>
+    <button class="btn auth-form__btn" type="submit">Register</button>
 
     {{ if .Errors.email}}
     <p class="auth-form__message auth-form__message-error">
@@ -649,7 +649,7 @@ func createHtmlFile(projectName string) {
     {{ end }}
 
     <p class="auth-form__type">Already have an account? <button class="btn btn-ghost" type="button"
-        hx-get="/auth/sign-in" hx-target="body">Register</button></p>
+        hx-get="/auth/sign-in" hx-target="body">Sign In</button></p>
   </form>
 </div>
 {{ end }}
